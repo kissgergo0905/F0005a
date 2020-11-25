4753dbc2a863dc485805e4ecf9894363a55ba4c8
kissgergo0905 <kiss.gergo0905@gmail.com>
2020-11-25 08:04:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+gondolt_szám = 7
+tipp = input('Melyik számra gondolok 1 és 10 között? ')
+tipp = int(tipp)
+if gondolt_szám == tipp: 
+    print('Bemehet')
+else: 
+    print('Hozzáférés elutasítva ')
